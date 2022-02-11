4e5650da47fca0337ec62b71920049d8b8f9e8a7
James Fricker <jimmyf98@hotmail.com>
2022-02-12 10:47:08 +1100

    Update test_flatmates_scraper.py

--- tests/test_flatmates_scraper.py
+++ tests/test_flatmates_scraper.py
@@ -1,8 +1,17 @@
-from lib import flatmates_scraper
+"""simple tests for flatmates"""
+import flatmates_scraper
 
-test_url = "https://flatmates.com.au/rooms/melbourne?page="
-getFlatmatesData = flatmates_scraper.GetFlatmatesData()
-test_data = getFlatmatesData.scrape_all_flatmates_info(test_url, 2)
 
-for t in test_data:
-    print(t)
+def test_soup_from_url():
+    """test if scraper returns data"""
+    test_url = "https://flatmates.com.au/rooms/melbourne?page="
+    get_flatmates_data = flatmates_scraper.soup_from_url(test_url)
+    assert len(get_flatmates_data) > 0
+
+def test_get_flatmates_max_page():
+    """test if max page is 220"""
+    assert 180 <= flatmates_scraper.get_flatmates_max_page(
+        "https://flatmates.com.au/rooms/melbourne") <= 250
+
+
+    
