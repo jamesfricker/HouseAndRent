2ead729f5ff71128d6dc3562b5ccf3cf00ac27f4
James Fricker <jimmyf98@hotmail.com>
2022-05-28 20:58:45 +1000

    refactor

---
+++ src/load_to_db.py
@@ -0,0 +1,44 @@
+"""file for loading data in sqlite db"""
+import sqlite3
+import os
+import csv
+
+def load_flatmates_data_to_db(flatmates_house_info,labels):
+    """load data in sqlite database"""
+    connection = sqlite3.connect('src/db/flatmates_data.db')
+    cursor = connection.cursor()
+    insert_row_to_db = []
+    for i in labels:
+        insert_row_to_db.append(flatmates_house_info[i])
+
+    # update
+    cursor.execute("UPDATE flatmates_rent_listings " +
+                    "SET date=date " +
+                    "WHERE flatmates_id='" +str(flatmates_house_info["flatmates_id"]) + "';")
+    try:
+        cursor.execute("INSERT OR IGNORE INTO flatmates_rent_listings " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?,?,?)", insert_row_to_db)
+        print("Successfully insert or updated listing " +
+            str(flatmates_house_info["flatmates_id"]) + " into db")
+    except Exception as e:
+        print("Error inserting listing " + \
+            str(flatmates_house_info["flatmates_id"]) + \
+            " into db" + str(e))
+
+    connection.commit()
+    connection.close()
+
+def read_csv_extract():
+    """read csv file and extract data"""
+    output_location = os.path.join(os.getcwd(), "output")
+    print(output_location)
+    files = os.listdir(output_location)
+    latest_extract = files[-1]
+    print(latest_extract)
+    return csv.DictReader(open(f"output/{latest_extract}", "r",encoding='utf-8'))
+
+if __name__ == "__main__":
+    output_reader = read_csv_extract()
+    for row in output_reader:
+        load_flatmates_data_to_db(row,output_reader.fieldnames)
+    print("finished loading data to db")
