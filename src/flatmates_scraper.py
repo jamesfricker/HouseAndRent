1d1fc291fb6a3d01d0567137a53b0137910ba4c5
James Fricker <jimmyf98@hotmail.com>
2022-07-23 14:05:40 +1000

    formatting

--- src/flatmates_scraper.py
+++ src/flatmates_scraper.py
@@ -7,32 +7,44 @@ from bs4 import BeautifulSoup
 import requests
 
 
-labels = ["flatmates_id", "url", "suburb", "city", "price", "price_includes_bills",
-        "rooms_available", "house_type", "bedroom_count",
-        "bathroom_count", "people_count","date"]
+labels = [
+    "flatmates_id",
+    "url",
+    "suburb",
+    "city",
+    "price",
+    "price_includes_bills",
+    "rooms_available",
+    "house_type",
+    "bedroom_count",
+    "bathroom_count",
+    "people_count",
+    "date",
+]
 
-class GetFlatmatesData():
+
+class GetFlatmatesData:
     """get data for listed homes from flatmates"""
+
     def __init__(self) -> None:
         pass
 
-    def scrape_flatmates_page_info(self,url):
-        """ scrape an entire page on FM """
+    def scrape_flatmates_page_info(self, url):
+        """scrape an entire page on FM"""
         all_house_info = []
         page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(page.content, "html.parser")
         for div in soup.find_all(class_="styles__listingTileBox___2r9Cb"):
             one_house_info = self.get_one_house_info(div)
-            if one_house_info: # not None
+            if one_house_info:  # not None
                 all_house_info.append(one_house_info)
                 # load_to_db.load_flatmates_data_to_db(one_house_info,labels)
         return all_house_info
 
-
-    def get_one_house_info(self,div):
+    def get_one_house_info(self, div):
         """get information for a single home from the page div"""
         # get house flatmates_id
-        url = div.find(class_="styles__contentBox___37_w9").get('href')
+        url = div.find(class_="styles__contentBox___37_w9").get("href")
         flatmates_id = url.split("-")[-1]
 
         # get suburb
@@ -43,13 +55,13 @@ class GetFlatmatesData():
         # get price
         text = div.get_text(strip=True)
         does_include_bills = text.find("bills") != -1
-        price = text.split("$")[-1].split("/")[0].replace(",","")
+        price = text.split("$")[-1].split("/")[0].replace(",", "")
 
         # fix price if low-high format
         if price.find("-") != -1:
             low = price.split("-")[0]
             high = price.split("-")[1]
-            price = str((int(low)+int(high))/2).split('.',maxsplit=1)[0]
+            price = str((int(low) + int(high)) / 2).split(".", maxsplit=1)[0]
 
         # fix price if empty
         if price == "":
@@ -58,7 +70,7 @@ class GetFlatmatesData():
         # get house features
         house_features = []
         for features in div.find_all(class_="styles__propertyFeature___uH480"):
-            feature = features.find('p').text
+            feature = features.find("p").text
             house_features.append(feature)
         # assign house features
         bedroom_count = house_features[0]
@@ -69,7 +81,7 @@ class GetFlatmatesData():
         room_type = div.find(class_="styles__roomInfo___1BEdy").text
         # certain room types don't display number of rooms rooms available
         # Studios and Whole House Available
-        whole_property_types = ["whole", "studio","granny"]
+        whole_property_types = ["whole", "studio", "granny"]
         if any(x in room_type.lower() for x in whole_property_types):
             rooms_available = bedroom_count
             house_type = room_type
@@ -90,25 +102,24 @@ class GetFlatmatesData():
                 "bedroom_count": int(bedroom_count),
                 "bathroom_count": int(bathroom_count),
                 "people_count": int(people_count),
-                "date": date.today().strftime("%d-%m-%Y")
-
+                "date": date.today().strftime("%d-%m-%Y"),
             }
             return house_information
 
         except Exception as e_error:
             print(e_error, "ERROR writing to error log")
-            error_logs = open("logs/errors.txt", "a",encoding='utf-8')
+            error_logs = open("logs/errors.txt", "a", encoding="utf-8")
             error_logs.write(datetime.now().strftime("%d %m %y %H %M"))
-            error_logs.write(" error scraping " + str(url) +"\n")
+            error_logs.write(" error scraping " + str(url) + "\n")
             error_logs.close()
             return None
 
-    def write_dict_to_csv(self,csv_name, dict_arr):
+    def write_dict_to_csv(self, csv_name, dict_arr):
         """
         write a dict to csv with the given labels
         """
         try:
-            with open(csv_name, 'w',encoding='UTF-8') as _:
+            with open(csv_name, "w", encoding="UTF-8") as _:
                 writer = csv.DictWriter(_, fieldnames=labels)
                 writer.writeheader()
                 for city in dict_arr:
@@ -118,51 +129,67 @@ class GetFlatmatesData():
             print("I/O error")
         return True
 
-
-    def scrape_all_flatmates_info(self,base_url, pages):
-        """ scrape info from all of FM """
+    def scrape_all_flatmates_info(self, base_url, pages):
+        """scrape info from all of FM"""
         dict_arr = []
-        for i in range(1, pages+1):
+        for i in range(1, pages + 1):
             time.sleep(2)
             this_url = base_url + str(i)
             # houses_info = scrape_flatmates_house_info(this_url)
             for house in self.scrape_flatmates_page_info(this_url):
                 dict_arr.append(house)
             print("finished scraping ", this_url)
-            print("percentage complete: ", (i/(pages)*100))
+            print("percentage complete: ", (i / (pages) * 100))
         return dict_arr
 
-
-    def write_house_data_to_csv(self,house_data):
-        """ write house data to csv """
+    def write_house_data_to_csv(self, house_data):
+        """write house data to csv"""
         date_formatted = date.today().strftime("%Y_%m_%d")
         output_folder_location = "output"
-        self.write_dict_to_csv(f"{output_folder_location}/raw_{date_formatted}.csv", house_data)
+        self.write_dict_to_csv(
+            f"{output_folder_location}/raw_{date_formatted}.csv", house_data
+        )
 
 
 def soup_from_url(url):
     """get bs4 from a url"""
     page = requests.get(url)
-    return BeautifulSoup(page.content, 'html.parser')
+    return BeautifulSoup(page.content, "html.parser")
+
 
 def get_flatmates_max_page(base_url):
     """find the maximum page from flatmates"""
     soup = soup_from_url(base_url)
-    pages_nums = soup.find_all(class_="styles__pageLink___2f9vK styles__otherPage___oBFSI")
+    pages_nums = soup.find_all(
+        class_="styles__pageLink___2f9vK styles__otherPage___oBFSI"
+    )
     return int(pages_nums[-1].text)
 
+
 def main():
-    """ run the program """
+    """run the program"""
     flatmates_data = GetFlatmatesData()
-    cities = ["sydney", "melbourne", "brisbane", "perth", "adelaide", "canberra","hobart","darwin"]
+    cities = [
+        "sydney",
+        "melbourne",
+        "brisbane",
+        "perth",
+        "adelaide",
+        "canberra",
+        "hobart",
+        "darwin",
+    ]
     all_listings = []
     for city in cities:
         base_url = "https://flatmates.com.au/rooms/" + city + "/newest"
         num_pages = get_flatmates_max_page(base_url)
         print(f"scraping {city} with {num_pages} pages")
-        all_listings.append(flatmates_data.scrape_all_flatmates_info(base_url+"?page=",num_pages))
+        all_listings.append(
+            flatmates_data.scrape_all_flatmates_info(base_url + "?page=", num_pages)
+        )
 
     flatmates_data.write_house_data_to_csv(all_listings)
+
 
 if __name__ == "__main__":
     main()
--- src/load_to_db.py
+++ src/load_to_db.py
@@ -3,33 +3,46 @@ import sqlite3
 import os
 import csv
 
-def load_flatmates_data_to_db(flatmates_house_info,labels):
+
+def load_flatmates_data_to_db(flatmates_house_info, labels):
     """load data in sqlite database"""
-    connection = sqlite3.connect('src/db/flatmates_data.db')
+    connection = sqlite3.connect("src/db/flatmates_data.db")
     cursor = connection.cursor()
     insert_row_to_db = []
     for i in labels:
         insert_row_to_db.append(flatmates_house_info[i])
 
     # update
-    cursor.execute(f"""
-    UPDATE flatmates_rent_listings 
-    SET date=date 
+    cursor.execute(
+        f"""
+    UPDATE flatmates_rent_listings
+    SET date=date
     WHERE flatmates_id='{str(flatmates_house_info["flatmates_id"])}';
-    """)
+    """
+    )
     try:
-        cursor.execute("""
+        cursor.execute(
+            """
         INSERT OR IGNORE INTO flatmates_rent_listings
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
-        """, insert_row_to_db)
-        print("Successfully insert or updated listing " +
-            str(flatmates_house_info["flatmates_id"]) + " into db")
+        """,
+            insert_row_to_db,
+        )
+        print(
+            "Successfully insert or updated listing "
+            + str(flatmates_house_info["flatmates_id"])
+            + " into db"
+        )
     except Exception as exception:
-        print("Error inserting listing " + \
-            str(flatmates_house_info["flatmates_id"]) + \
-            " into db" + str(exception))
+        print(
+            "Error inserting listing "
+            + str(flatmates_house_info["flatmates_id"])
+            + " into db"
+            + str(exception)
+        )
     connection.commit()
     connection.close()
+
 
 def read_csv_extract():
     """read csv file and extract data"""
@@ -39,10 +52,11 @@ def read_csv_extract():
     files.sort()
     latest_extract = files[-1]
     print(latest_extract)
-    return csv.DictReader(open(f"output/{latest_extract}", "r",encoding='utf-8'))
+    return csv.DictReader(open(f"output/{latest_extract}", "r", encoding="utf-8"))
+
 
 if __name__ == "__main__":
     output_reader = read_csv_extract()
     for row in output_reader:
-        load_flatmates_data_to_db(row,output_reader.fieldnames)
+        load_flatmates_data_to_db(row, output_reader.fieldnames)
     print("finished loading data to db")
